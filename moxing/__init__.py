70f88b998df02457d586ce5c537c1e82ac59e0d8
akino <6130092+cycleuser@users.noreply.github.com>
2026-05-19 17:22:38 +0800

    update

--- moxing/__init__.py
+++ moxing/__init__.py
@@ -29,7 +29,7 @@ Quick start:
     tq = TurboQuant(TurboQuantConfig(bits_per_channel=3.5))
 """
 
-__version__ = "0.1.36"
+__version__ = "0.1.38"
 
 from moxing.binaries import (
     BinaryManager,
