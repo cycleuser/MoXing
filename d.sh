e66b494dd8f2a3ce375b6d9d0f2770c94a2dc334
akino <6130092+cycleuser@users.noreply.github.com>
2026-05-25 13:13:46 +0800

    adding d.ssh tto ddownloaad mmodels

---
+++ d.sh
@@ -0,0 +1,64 @@
+
+
+modelscope download --model Jackrong/Qwopus3.5-27B-v3-GGUF Qwopus3.5-27B-v3-Q4_K_M.gguf --local_dir ./
+
+
+modelscope download --model Jackrong/Qwopus3.5-27B-v3-GGUF Qwopus3.5-27B-v3-Q5_K_M.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwopus3.5-27B-v3-GGUF Qwopus3.5-27B-v3-Q6_K.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwen3.5-9B-DeepSeek-V4-Flash-GGUF Qwen3.5-9B-DeepSeek-V4-Flash-BF16.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwen3.5-9B-DeepSeek-V4-Flash-GGUF Qwen3.5-9B-DeepSeek-V4-Flash-Q4_K_M.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwen3.5-9B-DeepSeek-V4-Flash-GGUF Qwen3.5-9B-DeepSeek-V4-Flash-Q5_K_M.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwen3.5-9B-DeepSeek-V4-Flash-GGUF Qwen3.5-9B-DeepSeek-V4-Flash-Q6_K.gguf  --local_dir ./
+
+
+modelscope download --model Tesslate/OmniCoder-2-9B-GGUF omnicoder-2-9b-bf16.gguf --local_dir ./
+
+
+modelscope download --model Jackrong/Qwopus3.5-9B-Coder-MTP-GGUF   Qwopus3.5-9B-Coder-MTP-BF16.gguf  --local_dir ./
+
+
+modelscope download --model Jackrong/Qwopus3.5-9B-Coder-MTP-GGUF   Qwopus3.5-9B-Coder-MTP-Q4_K_M.gguf  --local_dir ./
+
+
+modelscope download --model Jackrong/Qwopus3.5-9B-Coder-MTP-GGUF   Qwopus3.5-9B-Coder-MTP-Q5_K_M.gguf  --local_dir ./
+
+
+
+
+modelscope download --model Jackrong/Qwopus3.5-27B-v3-GGUF Qwopus3.5-27B-v3-Q4_K_M.gguf --local_dir ./
+
+
+modelscope download --model Jackrong/Qwopus3.5-27B-v3-GGUF Qwopus3.5-27B-v3-Q5_K_M.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwopus3.5-27B-v3-GGUF Qwopus3.5-27B-v3-Q6_K.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwen3.5-9B-DeepSeek-V4-Flash-GGUF Qwen3.5-9B-DeepSeek-V4-Flash-BF16.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwen3.5-9B-DeepSeek-V4-Flash-GGUF Qwen3.5-9B-DeepSeek-V4-Flash-Q4_K_M.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwen3.5-9B-DeepSeek-V4-Flash-GGUF Qwen3.5-9B-DeepSeek-V4-Flash-Q5_K_M.gguf --local_dir ./
+
+modelscope download --model Jackrong/Qwen3.5-9B-DeepSeek-V4-Flash-GGUF Qwen3.5-9B-DeepSeek-V4-Flash-Q6_K.gguf  --local_dir ./
+
+
+modelscope download --model Tesslate/OmniCoder-2-9B-GGUF omnicoder-2-9b-bf16.gguf --local_dir ./
+
+
+modelscope download --model Jackrong/Qwopus3.5-9B-Coder-MTP-GGUF   Qwopus3.5-9B-Coder-MTP-BF16.gguf  --local_dir ./
+
+
+modelscope download --model Jackrong/Qwopus3.5-9B-Coder-MTP-GGUF   Qwopus3.5-9B-Coder-MTP-Q4_K_M.gguf  --local_dir ./
+
+
+modelscope download --model Jackrong/Qwopus3.5-9B-Coder-MTP-GGUF   Qwopus3.5-9B-Coder-MTP-Q5_K_M.gguf  --local_dir ./
+
+
+
+
+
+
